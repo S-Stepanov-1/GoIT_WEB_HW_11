d953eaa07653e180e71fdfdc5a58fc2e3ed26914
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-08-10 21:02:46 +0300

    add get{id} & delete method

--- notes/repository/notes.py
+++ notes/repository/notes.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from sqlalchemy.orm import Session
-
+from fastapi import HTTPException, status
 from notes.database.models import Note  # noqa
 from notes.schemas import NoteModel, NoteResponse
 
@@ -15,3 +15,20 @@ async def create_note(body: NoteModel, db: Session) -> Note:
     db.add(note)
     db.commit()
     return note
+
+
+async def get_note(note_id: int, db: Session) -> Note:
+    note = db.query(Note).filter(Note.id == note_id).first()
+    return note
+
+
+async def delete_note(note_id: int, db: Session):
+    note = db.query(Note).filter_by(id=note_id).first()
+    if note is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="NOT FOUND",
+        )
+    db.delete(note)
+    db.commit()
+    return f"{note.first_name} {note.last_name} was deleted"
--- notes/routes/notes.py
+++ notes/routes/notes.py
@@ -4,7 +4,6 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 
 from notes.database.db_connect import get_db
-from notes.database.models import Note
 from notes.repository import notes
 from notes.schemas import NoteResponse, NoteModel
 
@@ -16,6 +15,19 @@ async def read_notes(skip: int = 0, limit: int = 10, db: Session = Depends(get_d
     return await notes.get_notes(skip, limit, db)
 
 
+@router.get("/{note_id}", response_model=NoteResponse)
+async def read_note(note_id: int, db: Session = Depends(get_db)):
+    note = await notes.get_note(note_id, db)
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+    return note
+
+
 @router.post("/", response_model=NoteResponse)
 async def create_note(body: NoteModel, db: Session = Depends(get_db)):
     return await notes.create_note(body, db)
+
+
+@router.delete("/{note_id}")
+async def delete_note(note_id: int, db: Session = Depends(get_db)):
+    return await notes.delete_note(note_id, db)
