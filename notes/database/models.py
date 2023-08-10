ad5d590faccaa2f8de24b16bb690c51e8e7d9904
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-08-10 17:27:11 +0300

    add model & migrations

---
+++ notes/database/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.sqltypes import Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Note(Base):
+    __tablename__ = "notes"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(25), nullable=True)
+    phone_number = Column(String(12), nullable=False)
+    birthday = Column(Date, nullable=True)
+    position = Column(String(50), nullable=True)
